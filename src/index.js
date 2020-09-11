6642671708d493e0fd8e3c8ba19691d8602dced1
irlgabriel <radu.gabriel131@gmail.com>
2020-09-11 20:13:36 +0300

    finished dynamically generating html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 
+
 const displayController = (() => {
 
   // main element
-  const content = document.querySelector(".content")
-
+  const content = document.querySelector("#content")
+  console.log(content)
   // ---------- TOP DIV ----------
 
   // top div with image and nav
-  const top = document.createElement("div");
-  content.appendChild(top)
+  const topDiv = document.createElement("div");
+  content.appendChild(topDiv)
 
-  top.classList.add("top");
+  topDiv.classList.add("top");
 
   // top nav
   const nav = document.createElement("nav");
-  top.appendChild(nav)
+  topDiv.appendChild(nav)
 
   // nav items
   let p = document.createElement("p");
@@ -22,24 +23,27 @@ const displayController = (() => {
   p.innerHTML = "Contact";
   nav.appendChild(p);
 
+  p = document.createElement("p");
   p.innerHTML = "Menu";
   nav.appendChild(p)
   
+  p = document.createElement("p");
   p.innerHTML = "Career";
   nav.appendChild(p)
 
+  p = document.createElement("p");
   p.innerHTML = "Locations";
   nav.appendChild(p)
 
+  p = document.createElement("p");
   p.innerHTML = "Reservations";
   nav.appendChild(p);
-
 
   //top hero
 
   const topHero = document.createElement("div");
   topHero.classList.add("top-hero");
-  top.appendChild(topHero)
+  topDiv.appendChild(topHero)
 
   let temp = document.createElement("p")
 
@@ -53,13 +57,32 @@ const displayController = (() => {
 
   const bottom = document.createElement("div");
   bottom.classList.add("bottom")
-
   for(let i = 1; i <= 3; i ++) {
     const box = document.createElement("div");
     box.classList.add(`flex-box-${i}`);
-    box.appendChild(document.createElement("section").classList.add("bottom-hero"));
+    const section = document.createElement("section")
+    section.classList.add("bottom-hero")
+    box.appendChild(section);
+    bottom.appendChild(box)
   }
-
   content.appendChild(bottom)
 
+  const box1 = document.querySelector(".flex-box-1")
+  const box2 = document.querySelector(".flex-box-2")
+  const box3 = document.querySelector(".flex-box-3")
+  console.log(`${box1}, ${box2}, ${box3}`);
+  p = document.createElement("p");
+
+  p.innerHTML = "Menu"
+  box1.querySelector("section").appendChild(p)
+
+  p = document.createElement("p");
+  p.innerHTML = "Career"
+  box2.querySelector("section").appendChild(p)
+
+  p = document.createElement("p");
+  p.innerHTML = "Reservations"
+  box3.querySelector("section").appendChild(p)
+
+ 
 })()
